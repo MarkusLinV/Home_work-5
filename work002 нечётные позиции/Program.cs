61c72666668412ca464c98a9f2731a9900a46953
MarkusLinV <maksim.lin.00@mail.ru>
2023-09-18 16:54:01 +0200

    задание 2

---
+++ work002 нечётные позиции/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+int[] getRandomArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+int SumNotEven(int[] array)
+{
+    int result = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+        result = result + array[i];
+    }
+    return result;
+}
+
+int[] array = getRandomArray(6, 1, 10);
+
+Console.WriteLine(string.Join(", ", array));
+
+int result = SumNotEven(array);
+
+Console.WriteLine($"Сумма всех элементов массива на нечётных позициях равна: {result}");
