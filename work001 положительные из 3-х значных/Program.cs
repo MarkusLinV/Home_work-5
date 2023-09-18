4e1e9c75cd1122bd24cbc8c1fa869ff4b6f7fb57
MarkusLinV <maksim.lin.00@mail.ru>
2023-09-18 16:42:58 +0200

    первое задание и гитигноре

---
+++ work001 положительные из 3-х значных/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.Clear();
+int[] getRandomArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+int LookEven(int[] arrey)
+{
+    int count = 0;
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        if (arrey[i] % 2 == 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+int[] array = getRandomArray(5, 100, 999);
+
+Console.WriteLine(string.Join(", ", array));
+
+int result = LookEven(array);
+
+Console.WriteLine($"Колл-во чётных числе в массиве: {result}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
